7631b1567918bd95f321a15d67d0a634db808bc4
vimigueloli <vipeso2016@gmail.com>
2021-08-23 19:59:17 -0300

    ajuste no deploy

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -11,7 +11,6 @@ export function Display(props){
     if(props.prev != undefined){
         let audio = document.getElementById('audio')
         if(audio != undefined){
-            
             audio.load()
             audio.play()
         } 
